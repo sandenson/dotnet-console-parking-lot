27f7ca6a345703b95582537aae75b1e44b8fe8ff
sandenson <lgabrielsandes@gmail.com>
2023-10-10 18:03:33 -0300

    feat: Everything's working.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,22 +1,48 @@
-﻿using DesafioFundamentos.Models;
+﻿using System.ComponentModel;
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+void PressioneParaContinuar () {
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+// Baseado em https://stackoverflow.com/a/35554808
+void ColetarEConverterValor<T> (out T valor, string mensagem) {
+    bool sucesso = false;
+    valor = default(T);
+
+    while (!sucesso) {
+        TypeConverter conversor = TypeDescriptor.GetConverter(typeof(T));
+        
+        if (conversor != null) {
+            try {
+                Console.WriteLine(mensagem);
+                valor = (T)conversor.ConvertFromString(Console.ReadLine());
+                sucesso = true;
+            } catch (Exception) {
+                Console.WriteLine("Valor inválido! Tente novamente.");
+                PressioneParaContinuar();
+            }
+        } else {
+            throw new ArgumentException("Tipo de valor inválido", typeof(T).Name);
+        }
+    }
+
+    Console.WriteLine($"\nSUCESSO PORRA\n{valor} - {typeof(T).Name}\n");
+}
+
+ColetarEConverterValor(out decimal precoInicial, "Digite o preço inicial:");
+
+ColetarEConverterValor(out decimal precoPorHora, "Agora digite o preço por hora:");
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
-string opcao = string.Empty;
 bool exibirMenu = true;
 
 // Realiza o loop do menu
@@ -52,8 +78,7 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+    PressioneParaContinuar();
 }
 
 Console.WriteLine("O programa se encerrou");
